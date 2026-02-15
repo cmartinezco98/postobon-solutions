ae30f89b159da9fa66cf6a366e4579e2b1d2cf08
cmartinezco98 <cmartinezcodev@gmail.com>
2026-02-14 21:49:42 -0500

    Se crea api-dist

--- apps/api-dist/src/app/app.module.ts
+++ apps/api-dist/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-// Configuraciones
-import { ConfigModule} from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true ,     
-      // envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`, 
-})],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot({
+  })],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- apps/api-dist/src/main.ts
+++ apps/api-dist/src/main.ts
@@ -2,21 +2,15 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-// Configuraciones
-import { ConfigService } from '@nestjs/config';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const configService = app.get(ConfigService);
-  // const prefixApi = configService.get('NAME_API') + '-' + configService.get('API_VERSION');
-  // //Configuracion de prefijo en la ruta de la API
-  // app.setGlobalPrefix(prefixApi);
-
-  app.setGlobalPrefix("api-dist/v1");
-  // const port = configService.get<number>('PORT_API') || 3000;
-  await app.listen(3000);
+  const globalPrefix = '';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT_API || 3000;
+  await app.listen(port);
+  Logger.log(process.env);
   Logger.log(
-    `🚀 Application is running on: http://localhost:3000/api-dist/v1`
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
